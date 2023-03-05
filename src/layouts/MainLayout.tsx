36a6de5cd8f737f0dc4a1d78c0830fc58b534919
Mateusz Sowa <mateusz.jan.sowa@gmail.com>
2023-03-05 19:44:20 +0100

    install and init tailwindcss

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -3,6 +3,7 @@ import { Outlet } from 'react-router-dom'
 export default function MainLayout() {
     return (
         <>
+            <h1 className="text-3xl font-semibold underline">Hello world!</h1>
             <Outlet />
         </>
     )
