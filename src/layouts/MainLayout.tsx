2282fc2eafc273121422fe473d26e436758a8d66
Mateusz Sowa <mateusz.jan.sowa@gmail.com>
2023-03-05 20:01:24 +0100

    config tailwind for CRA

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -3,7 +3,14 @@ import { Outlet } from 'react-router-dom'
 export default function MainLayout() {
     return (
         <>
-            <h1 className="text-3xl font-semibold underline">Hello world!</h1>
+            <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
+                <p className="text-3xl text-gray-700 font-bold mb-5">
+                    Welcome!
+                </p>
+                <p className="text-gray-500 text-lg">
+                    React and Tailwind CSS in action
+                </p>
+            </div>
             <Outlet />
         </>
     )
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,18 +8,9 @@ module.exports = {
             lg: '976px',
             xl: '1440px'
         },
-        colors: {
-            blue: '#1fb6ff',
-            pink: '#ff49db',
-            orange: '#ff7849',
-            green: '#13ce66',
-            'gray-dark': '#273444',
-            gray: '#8492a6',
-            'gray-light': '#d3dce6'
-        },
         fontFamily: {
-            sans: ['Graphik', 'sans-serif'],
-            serif: ['Merriweather', 'serif']
+            sans: ['sans-serif'],
+            serif: ['serif']
         },
         extend: {
             spacing: {
