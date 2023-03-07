06c0d3205ac83fdd0fef0ce52f2e7fac2fbf75b6
Artur Kempny <ak@fss.cc>
2023-03-07 11:10:50 +0100

    instal and config redux-toolkit

---
+++ src/slices/userSlice.ts
@@ -0,0 +1,34 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { RootState } from 'config/store';
+
+interface User {
+  id: string
+  name: string
+}
+
+interface UserState {
+  user: User | null
+}
+
+const initialState: UserState = {
+  user: null
+};
+
+export const userSlice = createSlice({
+  name: 'users',
+  initialState,
+  reducers: {
+    setLoggedUser: (state, action) => {
+      state.user = action.payload
+    },
+    clearLoggedUser: (state) => {
+      state.user = null
+    },
+  },
+});
+
+export const { setLoggedUser, clearLoggedUser } = userSlice.actions
+
+export const userState = (state: RootState) => state.user
+
+export default userSlice.reducer
