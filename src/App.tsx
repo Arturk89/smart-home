7b963c6f72ac05f1a80b94fd7dda7df3793a524a
Mateusz Sowa <mateusz.jan.sowa@gmail.com>
2023-03-07 19:13:50 +0100

    comment out tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import React, { Suspense } from 'react'
 import { RouterProvider } from 'react-router-dom'
 import { routesConfig } from './routes/routesConfig'
-import './App.css'
 
 function App() {
     return (
-        <div className="App">
-            <Suspense>
-                <RouterProvider router={routesConfig} />
-            </Suspense>
-        </div>
+        <Suspense>
+            <RouterProvider router={routesConfig} />
+        </Suspense>
     )
 }
 
