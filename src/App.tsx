4895c7200b4f3cf5dda7c096b6f3fc5a4e14c177
Artur Kempny <ak@fss.cc>
2023-03-09 13:41:59 +0100

    connect redux to appplication and create basic auth guard with redirect

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React, { Suspense } from 'react'
+import { Provider } from 'react-redux'
+import { store } from 'config/store'
 import { RouterProvider } from 'react-router-dom'
 import { routesConfig } from './routes/routesConfig'
 
 function App() {
     return (
-        <Suspense>
-            <RouterProvider router={routesConfig} />
-        </Suspense>
+        <Provider store={store}>
+            <Suspense>
+                <RouterProvider router={routesConfig} />
+            </Suspense>
+        </Provider>
     )
 }
 
--- src/components/Login/LoginInputs.tsx
+++ src/components/Login/LoginInputs.tsx
@@ -2,9 +2,25 @@ import { Button, Text, Title, Box, TextInput } from '@mantine/core'
 import { mailRegex } from 'config/regex'
 import { useForm } from '@mantine/form'
 import { inputs } from './constants/inputs'
+import { userState } from 'slices/userSlice'
+import { setLoggedUser } from 'slices/userSlice'
+import { useAppDispatch, useAppSelector } from 'hooks/reduxTools'
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { pathNames } from 'routes/routes'
+
+type LoginForm = {
+  email: string
+  password: string
+}
 
 export function LoginInputs() {
-    const form = useForm({
+    const dispatch = useAppDispatch()
+    const { user } = useAppSelector(userState)
+    const navigate = useNavigate()
+    const { DASHBOARD } = pathNames
+
+    const form = useForm<LoginForm>({
         initialValues: { email: '', password: '' },
 
         validate: {
@@ -13,6 +29,15 @@ export function LoginInputs() {
         }
     })
 
+    const submitLogin = (data: LoginForm) =>
+      dispatch(setLoggedUser({ id: 'nanana', name: data.email }))
+
+    useEffect(() => {
+      if (user) {
+        navigate(DASHBOARD.path, { replace: true })
+      }
+    }, [user])
+      
     return (
         <Box
             // usune te sxy :P
@@ -21,7 +46,7 @@ export function LoginInputs() {
                 borderRadius: '5px',
                 background: 'rgba(188, 196, 203, 0.75)'
             }}>
-            <form onSubmit={form.onSubmit(console.log)}>
+            <form onSubmit={form.onSubmit(submitLogin)}>
                 <Title order={1}>Log in to your account</Title>
                 {inputs.map((field) => (
                     <TextInput
