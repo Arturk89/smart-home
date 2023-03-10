0e7ea4b730f76b419a53f8fd6d46ba58612221ef
Artur Kempny <ak@fss.cc>
2023-03-10 13:08:26 +0100

    simplyfy useRedirect

--- src/hooks/useRedirect.ts
+++ src/hooks/useRedirect.ts
@@ -1,13 +1,17 @@
 import { useEffect } from 'react'
 import { useNavigate, useLocation } from 'react-router-dom'
 
-export function useRedirect(when: string, redirectTo: string, replaced?: boolean) {
-  const location = useLocation()
-  const navigate = useNavigate()
+export function useRedirect(
+    when: string,
+    redirectTo: string,
+    replace?: boolean
+) {
+    const location = useLocation()
+    const navigate = useNavigate()
 
-  useEffect(() => {
-    if (location.pathname === when) {
-      navigate(redirectTo, { replace: replaced })
-    }
-  }, [location])
+    useEffect(() => {
+        if (location.pathname === when) {
+            navigate(redirectTo, { replace })
+        }
+    }, [location])
 }
