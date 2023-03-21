929b6886737bf5f71495eb9aeaa239acf0818816
mateuszjansowa <mateusz.jan.sowa@gmail.com>
2023-03-21 12:38:49 +0100

    Refactor login form component with updated styles and layout

--- src/components/Login/components/PlainInputs.component.tsx
+++ src/components/Login/components/PlainInputs.component.tsx
@@ -10,20 +10,28 @@ type Props = {
 
 export function LoginPlainInputs({ form, submitLogin }: Props) {
     return (
-        <Box>
-            <form onSubmit={form.onSubmit(submitLogin)}>
-                <Title order={1}>Log in to your account</Title>
-                {inputs.map((field) => (
-                    <TextInput
-                        key={field.name}
-                        label={field.label}
-                        withAsterisk={field.required}
-                        {...form.getInputProps(field.name)}
-                    />
-                ))}
-                <Box>
+        <Box className="border-2 border-slate-300 rounded-lg px-5 py-10">
+            <form
+                onSubmit={form.onSubmit(submitLogin)}
+                className="flex flex-col gap-10">
+                <Title
+                    className="font-light text-slate-100 text-left"
+                    order={1}>
+                    Log in to your account
+                </Title>
+                <Box className="flex flex-col gap-5">
+                    {inputs.map((field) => (
+                        <TextInput
+                            key={field.name}
+                            label={field.label}
+                            withAsterisk={field.required}
+                            {...form.getInputProps(field.name)}
+                        />
+                    ))}
+                </Box>
+                <Box className="flex gap-5 justify-end">
                     <Button type="submit">
-                        <Text className="text-gray-900">Log in</Text>
+                        <Text>Log in</Text>
                     </Button>
                     <Button>
                         <Text>Log in with PIN</Text>
