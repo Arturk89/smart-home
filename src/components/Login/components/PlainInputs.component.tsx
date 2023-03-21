edc1b7adc3fec969a824b5ab2edb67825f20ab15
mateuszjansowa <mateusz.jan.sowa@gmail.com>
2023-03-21 20:48:55 +0100

    Refactor LoginPlainInputs component to improve readability and simplify styles

--- src/components/Login/components/PlainInputs.component.tsx
+++ src/components/Login/components/PlainInputs.component.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Text, TextInput, Title } from '@mantine/core'
+import { Box, Button, TextInput, Title } from '@mantine/core'
 import { inputs } from '../constants/inputs'
 import { UseFormReturnType } from '@mantine/form'
 import { LoginForm } from '../PlainInputs.service'
@@ -10,16 +10,12 @@ type Props = {
 
 export function LoginPlainInputs({ form, submitLogin }: Props) {
     return (
-        <Box className="border-2 border-slate-300 rounded-lg px-5 py-10">
+        <Box className="border rounded-lg px-6 py-8">
             <form
                 onSubmit={form.onSubmit(submitLogin)}
-                className="flex flex-col gap-10">
-                <Title
-                    className="font-light text-slate-100 text-left"
-                    order={1}>
-                    Log in to your account
-                </Title>
-                <Box className="flex flex-col gap-5">
+                className="flex flex-col gap-10 text-title">
+                <Title order={1}>Log in to your account</Title>
+                <Box className="flex flex-col gap-6">
                     {inputs.map((field) => (
                         <TextInput
                             key={field.name}
@@ -29,12 +25,10 @@ export function LoginPlainInputs({ form, submitLogin }: Props) {
                         />
                     ))}
                 </Box>
-                <Box className="flex gap-5 justify-end">
-                    <Button type="submit">
-                        <Text>Log in</Text>
-                    </Button>
-                    <Button>
-                        <Text>Log in with PIN</Text>
+                <Box className="flex gap-6 justify-end">
+                    <Button variant="outline">Log in with PIN</Button>
+                    <Button variant="gradient" type="submit">
+                        Log in
                     </Button>
                 </Box>
             </form>
