cefccfeaa610e03ac696a154af288c56c9e0f5e6
mateuszjansowa <mateusz.jan.sowa@gmail.com>
2023-03-21 20:48:10 +0100

    Add Emotion cache to MantineProvider and update theme in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { MantineProvider } from '@mantine/core'
+import { MantineProvider, createEmotionCache } from '@mantine/core'
+import { globalTheme } from 'theme/global'
 import 'tailwindcss/tailwind.css'
 import './index.css'
+
+const emotionCache = createEmotionCache({
+    key: 'mantine',
+    prepend: false
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
     <React.StrictMode>
-        <MantineProvider withGlobalStyles>
+        <MantineProvider
+            emotionCache={emotionCache}
+            withGlobalStyles
+            withNormalizeCSS
+            theme={globalTheme}>
             <App />
         </MantineProvider>
     </React.StrictMode>
