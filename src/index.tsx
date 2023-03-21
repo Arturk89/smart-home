eb548d35296b015f9c6809e24c57d6472a2f762f
mateuszjansowa <mateusz.jan.sowa@gmail.com>
2023-03-21 12:10:47 +0100

    Remove MantineTheme and import Tailwind CSS in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { MantineProvider } from '@mantine/core'
-import { mantineTheme } from 'constants/mantineTheme'
-// commenting out the tailwind library
-// import 'tailwindcss/tailwind.css'
+import 'tailwindcss/tailwind.css'
 import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
     <React.StrictMode>
-        <MantineProvider withGlobalStyles withNormalizeCSS theme={mantineTheme}>
+        <MantineProvider withGlobalStyles>
             <App />
         </MantineProvider>
     </React.StrictMode>
